bf34ed516b50ceca2b66580ce8059df7f70cd54e
Gonzalo Rivero Baud <riverobaudg@gmail.com>
2022-01-31 09:52:04 -0300

    external_interfaces added with some interesting use cases

---
+++ external_interfaces/deeplearning.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+
+class Prediction():
+    # this module is in charge of machine learning prediction
+
+    def predict_product_price(product):
+        '''
+        this method predicts via Linear regresion algorithm whats an estimated best price for product p
+        '''
+        pass
+
+    def product_classification_by_season(orders):
+        '''
+        this method predicts via clustering algorithm whats the best season to sell each product from all orders. (Summer Autumn Winter Spring)
+        '''
+        pass
+
---
+++ external_interfaces/transport.py
@@ -0,0 +1,19 @@
+import logging
+
+
+class Transport():
+
+    # when an order is paid check the next transport departing to order destination
+    def chech_transport_to(destination):
+        '''
+        this method will chech via external API or service whats the next transport to departure to the destination given by parameter
+        '''
+        pass
+
+    def estimated_time(transport, destination):
+        '''
+        this method will chech via external API service or internal calculation whats the estimated time transport t will get to destination d
+        '''
+        pass
+
+    
